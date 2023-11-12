31537804c3c181c925a0bbfcf9d574dd99eed5b7
igor-budnitski <96424046+igor-budnitski@users.noreply.github.com>
2023-11-12 20:53:37 +0200

    Commit

---
+++ manipulating-complex-objects.js
@@ -0,0 +1,24 @@
+const myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  {
+    "artist": "Johnny Cash",
+    "title": "Ride This Train",
+    "release_year": 1960,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
